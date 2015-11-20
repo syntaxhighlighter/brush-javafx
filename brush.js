18a8e40769de9e415ba703e215847bf6041ec7fa
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-11-20 10:24:54 -0800

    Updates for 2015
    * Removes CoffeeScript
    * Removes Grunt
    * Removes Karma
    * Uses Babel

---
+++ brush.js
@@ -0,0 +1,46 @@
+var BrushBase = require('brush-base');
+var regexLib = require('regex-lib');
+
+function Brush() {
+  // Contributed by Patrick Webster
+  // http://patrickwebster.blogspot.com/2009/04/javafx-brush-for-syntaxhighlighter.html
+  var datatypes = 'Boolean Byte Character Double Duration ' + 'Float Integer Long Number Short String Void';
+
+  var keywords = 'abstract after and as assert at before bind bound break catch class ' + 'continue def delete else exclusive extends false finally first for from ' + 'function if import in indexof init insert instanceof into inverse last ' + 'lazy mixin mod nativearray new not null on or override package postinit ' + 'protected public public-init public-read replace return reverse sizeof ' + 'step super then this throw true try tween typeof var where while with ' + 'attribute let private readonly static trigger';
+
+  this.regexList = [
+    {
+      regex: regexLib.singleLineCComments,
+      css: 'comments'
+    },
+    {
+      regex: regexLib.multiLineCComments,
+      css: 'comments'
+    },
+    {
+      regex: regexLib.singleQuotedString,
+      css: 'string'
+    },
+    {
+      regex: regexLib.doubleQuotedString,
+      css: 'string'
+    },
+    {
+      regex: /(-?\.?)(\b(\d*\.?\d+|\d+\.?\d*)(e[+-]?\d+)?|0x[a-f\d]+)\b\.?/gi,
+      css: 'color2'
+    },
+    {
+      regex: new RegExp(this.getKeywords(datatypes), 'gm'),
+      css: 'variable'
+    },
+    {
+      regex: new RegExp(this.getKeywords(keywords), 'gm'),
+      css: 'keyword'
+    }
+	];
+  this.forHtmlScript(regexLib.aspScriptTags);
+};
+
+Brush.prototype = new BrushBase();
+Brush.aliases = ['jfx', 'javafx'];
+module.exports = Brush;
